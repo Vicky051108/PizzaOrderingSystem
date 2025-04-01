e733da65668b60c0f7fad3f2fc43aae4e1245e5b
Vicky051108 <3299632485@qq.com>
2025-04-01 23:28:52 +0800

    Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        takeOrder();
-        PizzaOrderingSystem system = new PizzaOrderingSystem();
-        System.out.println(system.toString());
+ public static void main(String[] args) {
+        ExceptionDemo demo = new ExceptionDemo();
+        demo.displayChoices();
     }
-    
-}
+}  
